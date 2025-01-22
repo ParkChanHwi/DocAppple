24a5f1e3186582d709e00d8f93d0247ea1cc0a4d
Parkchanhwi <cksgnl0523@gmail.com>
2025-01-22 16:20:56 +0900

    Initial Commit

---
+++ DocAppple/ContentView.swift
@@ -0,0 +1,26 @@
+// image source by  icons8 : https://icons8.kr/icon/MNKxQwOqbO8D/apple-fruit, flaticon : https://www.flaticon.com/free-icon/open-book_5773837
+//  ContentView.swift
+//  DocAppple
+//
+//  Created by 박찬휘 on 1/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
---
+++ DocAppple/DocApppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocApppleApp.swift
+//  DocAppple
+//
+//  Created by 박찬휘 on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct DocApppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
